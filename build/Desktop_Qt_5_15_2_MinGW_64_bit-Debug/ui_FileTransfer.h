1d3fe8a89350c5c25ed8bf69cb315a143d70b67b
pai <eric@example.com>
2025-05-04 16:06:33 +0800

    添加文件传输功能及界面调整

--- build/Desktop_Qt_5_15_2_MinGW_64_bit-Debug/ui_FileTransfer.h
+++ build/Desktop_Qt_5_15_2_MinGW_64_bit-Debug/ui_FileTransfer.h
@@ -35,8 +35,10 @@ public:
     QLabel *label_3;
     QSpinBox *spinBox;
     QPushButton *pushButton;
-    QLabel *label_4;
-    QWidget *tab_2;
+    QToolButton *toolButton;
+    QComboBox *comboBox;
+    QLabel *label_9;
+    QWidget *ClientPage;
     QLabel *label_5;
     QLabel *label_6;
     QLineEdit *lineEdit;
@@ -44,9 +46,7 @@ public:
     QSpinBox *spinBox_2;
     QLabel *label_8;
     QPushButton *pushButton_2;
-    QLabel *label_9;
-    QComboBox *comboBox;
-    QToolButton *toolButton;
+    QLabel *label_4;
     QStatusBar *statusbar;
 
     void setupUi(QMainWindow *FileTransfer)
@@ -58,7 +58,7 @@ public:
         centralwidget->setObjectName(QString::fromUtf8("centralwidget"));
         tabWidget = new QTabWidget(centralwidget);
         tabWidget->setObjectName(QString::fromUtf8("tabWidget"));
-        tabWidget->setGeometry(QRect(9, -1, 751, 431));
+        tabWidget->setGeometry(QRect(0, 0, 751, 431));
         ServerPage = new QWidget();
         ServerPage->setObjectName(QString::fromUtf8("ServerPage"));
         label = new QLabel(ServerPage);
@@ -76,43 +76,43 @@ public:
         pushButton = new QPushButton(ServerPage);
         pushButton->setObjectName(QString::fromUtf8("pushButton"));
         pushButton->setGeometry(QRect(20, 120, 281, 31));
-        label_4 = new QLabel(ServerPage);
-        label_4->setObjectName(QString::fromUtf8("label_4"));
-        label_4->setGeometry(QRect(20, 190, 61, 21));
+        toolButton = new QToolButton(ServerPage);
+        toolButton->setObjectName(QString::fromUtf8("toolButton"));
+        toolButton->setGeometry(QRect(230, 220, 51, 21));
+        comboBox = new QComboBox(ServerPage);
+        comboBox->setObjectName(QString::fromUtf8("comboBox"));
+        comboBox->setGeometry(QRect(20, 220, 191, 22));
+        label_9 = new QLabel(ServerPage);
+        label_9->setObjectName(QString::fromUtf8("label_9"));
+        label_9->setGeometry(QRect(30, 180, 61, 21));
         tabWidget->addTab(ServerPage, QString());
-        tab_2 = new QWidget();
-        tab_2->setObjectName(QString::fromUtf8("tab_2"));
-        label_5 = new QLabel(tab_2);
+        ClientPage = new QWidget();
+        ClientPage->setObjectName(QString::fromUtf8("ClientPage"));
+        label_5 = new QLabel(ClientPage);
         label_5->setObjectName(QString::fromUtf8("label_5"));
         label_5->setGeometry(QRect(20, 20, 91, 21));
-        label_6 = new QLabel(tab_2);
+        label_6 = new QLabel(ClientPage);
         label_6->setObjectName(QString::fromUtf8("label_6"));
         label_6->setGeometry(QRect(20, 50, 51, 16));
-        lineEdit = new QLineEdit(tab_2);
+        lineEdit = new QLineEdit(ClientPage);
         lineEdit->setObjectName(QString::fromUtf8("lineEdit"));
         lineEdit->setGeometry(QRect(70, 50, 113, 20));
-        label_7 = new QLabel(tab_2);
+        label_7 = new QLabel(ClientPage);
         label_7->setObjectName(QString::fromUtf8("label_7"));
         label_7->setGeometry(QRect(20, 82, 41, 20));
-        spinBox_2 = new QSpinBox(tab_2);
+        spinBox_2 = new QSpinBox(ClientPage);
         spinBox_2->setObjectName(QString::fromUtf8("spinBox_2"));
         spinBox_2->setGeometry(QRect(70, 80, 81, 22));
-        label_8 = new QLabel(tab_2);
+        label_8 = new QLabel(ClientPage);
         label_8->setObjectName(QString::fromUtf8("label_8"));
         label_8->setGeometry(QRect(20, 110, 41, 16));
-        pushButton_2 = new QPushButton(tab_2);
+        pushButton_2 = new QPushButton(ClientPage);
         pushButton_2->setObjectName(QString::fromUtf8("pushButton_2"));
         pushButton_2->setGeometry(QRect(20, 130, 241, 31));
-        label_9 = new QLabel(tab_2);
-        label_9->setObjectName(QString::fromUtf8("label_9"));
-        label_9->setGeometry(QRect(20, 170, 61, 21));
-        comboBox = new QComboBox(tab_2);
-        comboBox->setObjectName(QString::fromUtf8("comboBox"));
-        comboBox->setGeometry(QRect(20, 200, 191, 22));
-        toolButton = new QToolButton(tab_2);
-        toolButton->setObjectName(QString::fromUtf8("toolButton"));
-        toolButton->setGeometry(QRect(230, 200, 51, 21));
-        tabWidget->addTab(tab_2, QString());
+        label_4 = new QLabel(ClientPage);
+        label_4->setObjectName(QString::fromUtf8("label_4"));
+        label_4->setGeometry(QRect(20, 190, 61, 21));
+        tabWidget->addTab(ClientPage, QString());
         FileTransfer->setCentralWidget(centralwidget);
         statusbar = new QStatusBar(FileTransfer);
         statusbar->setObjectName(QString::fromUtf8("statusbar"));
@@ -120,7 +120,7 @@ public:
 
         retranslateUi(FileTransfer);
 
-        tabWidget->setCurrentIndex(1);
+        tabWidget->setCurrentIndex(0);
 
 
         QMetaObject::connectSlotsByName(FileTransfer);
@@ -133,16 +133,16 @@ public:
         label_2->setText(QCoreApplication::translate("FileTransfer", "\347\212\266\346\200\201\357\274\232", nullptr));
         label_3->setText(QCoreApplication::translate("FileTransfer", "\347\233\221\345\220\254\347\253\257\345\217\243\357\274\232", nullptr));
         pushButton->setText(QCoreApplication::translate("FileTransfer", "\345\274\200\345\247\213\347\233\221\345\220\254", nullptr));
-        label_4->setText(QCoreApplication::translate("FileTransfer", "\346\226\207\344\273\266\346\216\245\346\224\266\345\214\272", nullptr));
+        toolButton->setText(QCoreApplication::translate("FileTransfer", "\351\200\211\346\213\251\346\226\207\344\273\266", nullptr));
+        label_9->setText(QCoreApplication::translate("FileTransfer", "\351\200\211\346\213\251\346\226\207\344\273\266", nullptr));
         tabWidget->setTabText(tabWidget->indexOf(ServerPage), QCoreApplication::translate("FileTransfer", "\346\234\215\345\212\241\345\231\250\347\253\257", nullptr));
         label_5->setText(QCoreApplication::translate("FileTransfer", "\345\256\242\346\210\267\347\253\257\346\216\247\345\210\266\351\235\242\346\235\277", nullptr));
         label_6->setText(QCoreApplication::translate("FileTransfer", "\346\234\215\345\212\241\345\231\250IP\357\274\232", nullptr));
         label_7->setText(QCoreApplication::translate("FileTransfer", "\347\253\257\345\217\243\345\217\267\357\274\232", nullptr));
         label_8->setText(QCoreApplication::translate("FileTransfer", "\347\212\266\346\200\201\357\274\232", nullptr));
         pushButton_2->setText(QCoreApplication::translate("FileTransfer", "\350\277\236\346\216\245\346\234\215\345\212\241\345\231\250", nullptr));
-        label_9->setText(QCoreApplication::translate("FileTransfer", "\351\200\211\346\213\251\346\226\207\344\273\266", nullptr));
-        toolButton->setText(QCoreApplication::translate("FileTransfer", "...", nullptr));
-        tabWidget->setTabText(tabWidget->indexOf(tab_2), QCoreApplication::translate("FileTransfer", "\345\256\242\346\210\267\347\253\257", nullptr));
+        label_4->setText(QCoreApplication::translate("FileTransfer", "\346\226\207\344\273\266\346\216\245\346\224\266\345\214\272", nullptr));
+        tabWidget->setTabText(tabWidget->indexOf(ClientPage), QCoreApplication::translate("FileTransfer", "\345\256\242\346\210\267\347\253\257", nullptr));
     } // retranslateUi
 
 };
