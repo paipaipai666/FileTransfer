815d5635e0082c944debc6af32fc5c537d32ad0e
pai <eric@example.com>
2025-05-04 20:01:40 +0800

    优化了外观

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,17 @@
 #include "FileTransfer.h"
 
 #include <QApplication>
+#include <QFile>
 #pragma comment(lib, "user32.lib")
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+
+    QFile styleFile("./style/main.qss");
+    styleFile.open(QFile::ReadOnly);
+    QString styleSheet = QLatin1String(styleFile.readAll());
+    a.setStyleSheet(styleSheet);
     FileTransfer w;
     w.show();
     return a.exec();
